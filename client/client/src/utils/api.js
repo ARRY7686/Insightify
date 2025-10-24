048cf09cd233b50889dc5202c790db448e960e4d
ARRY7686 <aadigupta2007@yahoo.com>
2025-10-24 23:33:23 +0530

    Update API base URL to include '/api' in the endpoint

--- client/client/src/utils/api.js
+++ client/client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base configuration
 const api = axios.create({
-  baseURL: 'https://insightify-yang.onrender.com/',
+  baseURL: 'https://insightify-yang.onrender.com/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
